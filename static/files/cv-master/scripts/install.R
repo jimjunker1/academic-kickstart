0befbd066389b17a834825bca37e70339dd9206d
jimjunker1 <james.junker1@gmail.com>
2020-04-07 18:00:13 -0500

    added CV workflow

---
+++ static/files/cv-master/scripts/install.R
@@ -0,0 +1,18 @@
+# R packages --------------------------------------------------------------
+if (!require(pacman)) install.packages("pacman"); library(pacman)
+
+remotes::install_github("ropensci/tic")
+p_load(tinytex)
+p_load(scholar)
+p_load_gh("ropensci/vitae")
+p_load(tidyverse)
+p_load(lubridate)
+p_load(here)
+p_load(readxl)
+p_load(glue)
+
+
+# Get latex ---------------------------------------------------------------
+
+## This needs to be run manually if LaTex is not installed locally.
+##tinytex::install_tinytex()
